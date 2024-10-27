10354e9bbce026cb7086b030964728def102ed1d
Hyeongseok Kim <102458207+NeoSelf1@users.noreply.github.com>
2024-10-27 20:44:24 +0900

    feat: 폰트패밀리 사양 일괄 수정 (#1)

--- Ddom/Extensions/Font+.swift
+++ Ddom/Extensions/Font+.swift
@@ -8,40 +8,47 @@ import Foundation
 import SwiftUI
 
 extension Font {
-    static let heading = Font.custom("Pretendard-Bold", size: 28)
+    static let heading1 = Font.custom("Pretendard-Bold", size: 28)
+    static let heading2 = Font.custom("Pretendard-Bold", size: 24)
+    
     static let title1 = Font.custom("Pretendard-Bold", size: 20)
     static let title2 = Font.custom("Pretendard-SemiBold", size: 18)
-    static let body1 = Font.custom("Pretendard-Bold", size: 16)
-    static let body2 = Font.custom("Pretendard-Medium", size: 16)
-    static let body3 = Font.custom("Pretendard-Medium", size: 14)
-    static let caption = Font.custom("Pretendard-Medium", size: 12)
+    
+    static let body1 = Font.custom("Pretendard-Bold", size: 18)
+    static let body2 = Font.custom("Pretendard-Medium", size: 18)
+    static let body3 = Font.custom("Pretendard-Bold", size: 16)
+    static let body4 = Font.custom("Pretendard-Medium", size: 16)
+    static let body5 = Font.custom("Pretendard-Medium", size: 14)
+    
+    static let caption1 = Font.custom("Pretendard-Medium", size: 12)
+    static let caption2 = Font.custom("Pretendard-Medium", size: 12)
     
     var lineSpacing: CGFloat {
         switch self {
-        case .heading:
+        case .heading1:
             return 42 - 28
+        case .heading2:
+            return 36 - 24
         case .title1:
             return 30 - 20
         case .title2:
             return 24 - 18
-        case .body1, .body2:
+        case .body1, .body2, .body3, .body4:
             return 24 - 16
-        case .body3:
+        case .body5:
             return 22 - 14
-        case .caption:
-            return 16 - 12
         default:
-            return 0
+            return 16 - 12
         }
     }
     
     var letterSpacing: CGFloat {
         switch self {
-        case .heading, .title1:
+        case .heading1, .heading2, .title1:
             return -1.5
         case .title2, .body1, .body2, .body3:
             return -0.25
-        case .caption:
+        case .caption1, .caption2:
             return 0
         default:
             return 0
---
+++ Ddom/Utilities/Route.swift
@@ -0,0 +1,11 @@
+//
+//  Route.swift
+//  Ddom
+//
+//  Created by Neoself on 10/27/24.
+//
+
+enum Route: Hashable {
+    case createAccount
+    case onboarding
+}
