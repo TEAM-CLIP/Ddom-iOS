861c6b891cbe3dd326d1917a3162db54a53281eb
Hyeongseok Kim <102458207+NeoSelf1@users.noreply.github.com>
2024-10-27 20:44:42 +0900

    feat: OnboardingView, CreateAccountView 화면 작업 (#1)

--- Ddom/View/OnboardingView.swift
+++ Ddom/View/OnboardingView.swift
@@ -9,80 +9,92 @@ import SwiftUI
 import UIKit
 
 struct OnboardingView: View {
+    @StateObject var viewModel: OnboardingViewModel = OnboardingViewModel()
+    
     var body: some View {
-        VStack(spacing: 0) {
-            Spacer().frame(height:56)
-            VStack(alignment: .leading, spacing: 2) {
-                Text("자주가는 가게라면?")
-                    .fontStyle(.heading)
-                    .frame(maxWidth: .infinity,alignment: .leading)
-                
-                Text("다른 사람보다 쉽게 가격에 이용할 수 있어요!")
-                    .fontStyle(.title2)
-                    .foregroundColor(.gray)
-            }
-            .padding(.horizontal, 16)
-            
-            Spacer().frame(height:20)
-            
-            TabView {
-                ForEach(0..<3) { index in
-                    SwipeableContentView(index: index)
+        NavigationStack(path: $viewModel.navigationPath) {
+            VStack(spacing: 0) {
+                Spacer().frame(height:56)
+                VStack(alignment: .leading, spacing: 2) {
+                    Text("또 오고싶은 가게라면?")
+                        .fontStyle(.heading1)
+                        .foregroundStyle(.gray10)
+                        .frame(maxWidth: .infinity,alignment: .leading)
+                    
+                    Text("또옴 가게를 등록하고 혜택을 받아보세요!")
+                        .fontStyle(.body2)
+                        .foregroundStyle(.gray5)
                 }
-            }
-            .tint(.gray7)
-            .foregroundStyle(.gray7)
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
-            .padding(.horizontal, 20)
-            
-            Spacer()
-            
-            VStack(spacing: 8) {
-                Button(action: {
-                    // Kakao 로그인 액션
-                }) {
-                    HStack {
-                        Image(systemName: "message.fill")
-                        Text("Kakao로 로그인")
+                .padding(.horizontal, 16)
+                .padding(.bottom, 20)
+                
+                TabView {
+                    ForEach(0..<3) { index in
+                        SwipeableContentView(index: index)
                     }
-                    .fontStyle(.body3)
-                    .foregroundStyle(.black)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(Color(hex:"FAE300"))
-                    .cornerRadius(8)
                 }
+                .tint(.gray7)
+                .foregroundStyle(.gray7)
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                .padding(.horizontal, 16)
                 
-                Button(action: {
-                    // Apple 로그인 액션
-                }) {
-                    HStack {
-                        Image(systemName: "apple.logo")
-                        Text("Apple로 로그인")
+                Spacer()
+                
+                VStack(spacing: 8) {
+                    Button(action: {
+                        viewModel.kakaoLogin()
+                    }) {
+                        HStack {
+                            Image(systemName: "message.fill")
+                            Text("Kakao로 로그인하기")
+                        }
+                        .fontStyle(.body5)
+                        .foregroundStyle(.gray10)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color(hex:"FAE300"))
+                        .cornerRadius(8)
                     }
-                    .fontStyle(.body3)
-                    .foregroundStyle(.white)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(Color.black)
-                    .cornerRadius(8)
+                    
+                    Button(action: {
+                        // Apple 로그인 액션
+                    }) {
+                        HStack {
+                            Image(systemName: "apple.logo")
+                            Text("Apple로 로그인하기")
+                        }
+                        .fontStyle(.body5)
+                        .foregroundStyle(.white)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.black)
+                        .cornerRadius(8)
+                    }
+                    
+                    Button(action: {
+                        // 로그인 없이 사용하기 액션
+                    }) {
+                        Text("로그인 없이 사용하기")
+                            .fontStyle(.caption)
+                            .foregroundStyle(.gray4)
+                            .padding(.top,8)
+                            .padding(.bottom,16)
+                    }
                 }
-                
-                Button(action: {
-                    // 로그인 없이 사용하기 액션
-                }) {
-                    Text("로그인 없이 사용하기")
-                        .fontStyle(.caption)
-                        .foregroundStyle(.gray4)
-                        .padding(.top,8)
-                        .padding(.top,16)
+                .padding(.horizontal, 20)
+                .padding(.bottom, 8)
+            }
+            .edgesIgnoringSafeArea(.bottom)
+            .onAppear{setupAppearance()}
+            .navigationDestination(for: Route.self) { route in
+                switch route {
+                case .createAccount:
+                    CreateAccountView()
+                default:
+                    CreateAccountView()
                 }
             }
-            .padding(.horizontal, 20)
-            .padding(.bottom, 40)
         }
-        .edgesIgnoringSafeArea(.bottom)
-        .onAppear{setupAppearance()}
     }
 }
 
@@ -97,7 +109,7 @@ struct SwipeableContentView: View {
     var body: some View {
         VStack {
             Text("스와이프 가능한 콘텐츠 \(index + 1)")
-                .fontStyle(.heading)
+                .fontStyle(.heading1)
                 .foregroundColor(.gray2)
         }
         .frame(maxWidth: .infinity, maxHeight: 389)
